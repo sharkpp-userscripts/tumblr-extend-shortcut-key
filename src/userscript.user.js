81e0f4a8e8f7ddb68baebff3ef1b895ab4639d78
sharkpp <touroku@sharkpp.net>
2015-05-03 15:41:42 +0900

    2015-05-03時点のダッシュボートに対応

--- src/userscript.user.js
+++ src/userscript.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name        Tumblr. extend shortcut key
 // @namespace   http://www.sharkpp.net/
-// @version     0.1
+// @version     0.2
 // @description Tumblr. extend shortcut key for blog select and reblog button
 // @author      sharkpp
-// @copyright   2014, sharkpp
+// @copyright   2014-2015, sharkpp
 // @license     MIT License
 // @include     https://www.tumblr.com/dashboard*
 // @include     https://www.tumblr.com/reblog/*
@@ -14,32 +14,70 @@
 // ==/UserScript==
 (function ()
 {
-    var blogs = [];
-    var actionPublishNow, actionQueue, actionDraft, postButton;
+    var getPostForm = function() {
+        var postForm = jQuery('[data-subview="postForm"]');
+        return 0 < postForm.length && 0 < postForm[0].childNodes.length
+               ? postForm[0] : null;
+    };
+    var isBlogSelectMenuVisibled = function() {
+        return 0 < jQuery('[data-js-tumblelogchoice]').length;
+    };
+    var isActionSelectMenuVisibled = function() {
+        return 0 < jQuery('[data-js-publish]').length;
+    };
+    var popupPostForm = function() {
+        var k = Tumblr.KeyCommands, i;
+        k.update_post_positions();
+        k.current_position = (window.pageYOffset || (document.documentElement && document.documentElement.scrollTop) ||
+                              document.body.scrollTop) + k.scroll_offset;
+        for (i in k.post_positions) {
+            if (k.check_offset(k.post_positions[i])) {
+                jQuery('[data-pageable="' + i + '"] [data-subview="reblog"]').click();
+            }
+        }
+    };
+    var selectBlog = function(n) {
+        var menuVisibled = isBlogSelectMenuVisibled();
+        if (!menuVisibled)
+            jQuery('[data-js-clickabletumbleloglabel]')[0].click();
+        jQuery('[data-js-tumblelogchoice]:eq(' + n + ')').click();
+        if (menuVisibled)
+            jQuery('[data-js-clickabletumbleloglabel]')[0].click();
+    };
+    var actionSelect = function(act){
+        var menuVisibled = isActionSelectMenuVisibled();
+        if (!menuVisibled)
+            jQuery('[data-js-clickablesavedropdown]')[0].click();
+        jQuery('[data-js-' + act + ']').click();
+        jQuery('[data-js-clickablesave]')[0].click();
+    };
     var onshortcutkey = function (e) {
             if (e.altKey) {
-                if (0x31 <= e.keyCode && e.keyCode <= 0x39) { // Alt + '1'
+                if (49 <= e.keyCode && e.keyCode <= 57) { // Alt + '1' ... Alt + '9'
                     var n = (e.keyCode - 0x30) - 1;
-                    if (blogs[n])
-                        blogs[n].click();
+                    selectBlog(n);
                     return false;
-                } else if (0x52 == e.keyCode) { // Alt + 'r'
-                    actionPublishNow.click();
-                    postButton.click();
+                } else if (82 == e.keyCode) { // Alt + 'r'
+                    actionSelect('publish');
                     return false;
-                } else if (0x45 == e.keyCode) { // Alt + 'e'
-                    actionQueue.click();
-                    postButton.click();
+                } else if (69 == e.keyCode) { // Alt + 'e'
+                    actionSelect('queue');
                     return false;
-                } else if (0x44 == e.keyCode) { // Alt + 'd
-                    actionDraft.click();
-                    postButton.click();
+                } else if (68 == e.keyCode) { // Alt + 'd'
+                    actionSelect('draft');
+                    return false;
+                } else if (80 == e.keyCode) { // Alt + 'p'
+                    actionSelect('private');
                     return false;
                 }
                 return true;
+//          } else if (82 == e.keyCode) { // 'r'
+//                popupPostForm();
+//                console.log('************');
+//                return false;
             }
         };
-    var onkeydown = function (e) {
+    var onkeydown = function (e) {//console.log(e);
             e = e || window.event; // for IE
             if (e.altKey) {
                 if (0xE5 == e.keyCode) {
@@ -47,61 +85,49 @@
                 } else {
                     return onshortcutkey(e);
                 }
-            }
-        };
-    var onkeyup = function (e) {
-            e = e || window.event; // for IE
-            if (e.altKey) {
+            } else {
                 return onshortcutkey(e);
             }
         };
     var addShortcutKeyElements = function (elm, keys) {
             for (var i = 0, s; s = keys[i]; i++) {
                 var span = document.createElement('span');
-                    span.style.cssText = ['border:1px solid #ccc',
+                    span.style.cssText = ['background-color:#fff',
+                                          'border:1px solid #ccc',
                                           'border-radius:5px',
                                           'border-width:1px 1px 5px 5px',
-                                          'padding:2px'].join(';');
+                                          'padding:1px'].join(';');
                     span.innerHTML = s;
                 elm.appendChild(span);
                 if (i + 1 < keys.length)
                     elm.appendChild(document.createTextNode(' + '));
             }
         };
-    var fn = function () {
-            var itemBlogs = document.evaluate('//*[@id="tumblelog_choices"]/div/div/ul/li', document,
-                                              null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-            if (itemBlogs.snapshotLength &&
-                2 == itemBlogs.snapshotItem(0).firstChild.childNodes.length)
-            {
-                // get elements
-                var items;
-                items = document.evaluate('//*[@id="post_options"]/div/div/ul/li[1]/div', document, null, 
-                                          XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-                actionPublishNow = items.snapshotLength ? items.snapshotItem(0) : actionPublishNow;
-                items = document.evaluate('//*[@id="post_options"]/div/div/ul/li[2]/div', document, null, 
-                                          XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-                actionQueue = items.snapshotLength ? items.snapshotItem(0) : actionQueue;
-                items = document.evaluate('//*[@id="post_options"]/div/div/ul/li[4]/div', document, null, 
-                                          XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-                actionDraft = items.snapshotLength ? items.snapshotItem(0) : actionQueue;
-                items = document.evaluate('//*[@id="create_post"]/button', document, null,
-                                          XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,  null);
-                postButton = items.snapshotLength ? items.snapshotItem(0) : postButton;
-                // display shortcut key
-                for (var i = 0, item; item = itemBlogs.snapshotItem(i); i++) {
+    var mo = new MutationObserver(function(mr){
+        if (isBlogSelectMenuVisibled() &&
+            !jQuery('[data-js-tumblelogchoice]').attr('_6588-attr2'))
+        {
+            jQuery('[data-js-tumblelogchoice]')
+                .attr('_6588-attr2', '1')
+                .each(function(i){
                     var div = document.createElement('div');
-                        div.style.cssText = ['margin-right:25px',
+                        div.style.cssText = ['margin-right:5px',
                                              'text-align:right',
                                              'height:100%',
-                                             'line-height:25px'].join(';');
-                        addShortcutKeyElements(div, ["alt", (i + 1).toString()]);
-                    item.firstChild.appendChild(div);
-                    // save blog list
-                    blogs[i] = item.firstChild;
-                }
-                for (var i = 0, item, list = [actionPublishNow, actionQueue, actionDraft],
-                         scList = ['R', 'E', 'D']; item = list[i]; i++) {
+                                             'line-height:25px',
+                                             'position:absolute',
+                                             'right:0',
+                                             'top:5px'].join(';');
+                    addShortcutKeyElements(div, ["alt", (i + 1).toString()]);
+                    jQuery(this).children('div').append(div);
+                });
+        }
+        if (isActionSelectMenuVisibled() &&
+            !jQuery('[data-js-publish]').attr('_6588-attr3'))
+        {
+            jQuery('[data-js-publish],[data-js-queue],[data-js-draft],[data-js-private]')
+                .attr('_6588-attr3', '1')
+                .each(function(i){
                     var div2 = document.createElement('div');
                         div2.style.cssText = ['text-align: right',
                                               'height: 100%',
@@ -109,26 +135,15 @@
                                               'position: absolute',
                                               'top: 0px',
                                               'right: 10px'].join(';');
-                        addShortcutKeyElements(div2, ["alt", scList[i]]);
+                    addShortcutKeyElements(div2, ["alt", ['R', 'E', 'D', 'P'][i]]);
                     var div = document.createElement('div');
                         div.style.cssText = ['width: 200px'].join(';');
                         div.appendChild(div2);
-                    item.appendChild(div);
-                }
-                // register shortcut key event
-                document.onkeydown = onkeydown;
-                document.onkeyup = onkeyup;
-                var fn2 = function () {
-                        var tinymce = document.getElementById('post_two_ifr');
-                        if (!tinymce)
-                            setTimeout(fn2, 500);
-                        else 
-                            tinymce.contentWindow.document.onkeydown = onkeydown;
-                            tinymce.contentWindow.document.onkeyup = onkeyup;
-                    };
-                setTimeout(fn2, 500);
-            }
-            setTimeout(fn, 1000);
-        };
-    setTimeout(fn, 1000);
+                    jQuery(this).append(div2)
+                        .parent().parent().css('width', '230px');
+                });
+        }
+    });
+    mo.observe(document.body, {childList: true, subtree:true});
+    document.onkeydown = onkeydown;
 })();
